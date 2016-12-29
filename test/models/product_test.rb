f370dbe2b5bd2dffb3c876100095ddc10dec9a12
JTRIPPIER <joetrippier@gmail.com>
2016-12-29 13:58:12 +0000

    Validate the product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,70 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "Price must be positive" do
+
+    product = products(:valid_book)
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  test "image url must have a valid extension" do
+    product = products(:valid_book)
+
+    acceptable = %w{file.png fred.jpg feed.JPG fight.2.gif fly.GiF}
+    bad = %w{file.pngg fred.txt feed.html fight.jpeg fly.ffGIFff}
+
+    acceptable.each do |name|
+      product.image_url = name
+      assert product.valid?, "#{name} should be valid"
+    end
+
+    bad.each do |name|
+      product.image_url = name
+      assert product.invalid?, "#{name} should be invalid"
+    end
+  end
+
+  test "title attribute must be unique" do
+    product = Product.new(title: products(:valid_book).title,
+                           description:"A duplicate book",
+                           price: 9.95,
+                           image_url: "bookbook.png")
+
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+
+  end
+
+  test "title attribute must be unique - i18n" do
+    product = Product.new(title: products(:valid_book).title,
+                           description:"A duplicate book",
+                           price: 9.95,
+                           image_url: "bookbook.png")
+
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+
+  end
+
 end
