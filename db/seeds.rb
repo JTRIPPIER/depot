9d5004c70848baba9e71938e27962551a8da25f4
JTRIPPIER <joetrippier@gmail.com>
2016-12-29 11:07:37 +0000

    Initial commit of depot example

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+products = Product.create([
+  {title: 'Milwaukee Drill Set', description: 'A set of two drills. Ideal for any DIY project', image_url:'assets/images/MilwaukeeDrillSet.jpeg', price:295.00},
+  {title: 'Bosch laser level', description: 'Make sure everything is level', image_url:'assets/images/BoschLaserLevel.jpeg', price:35.00}
+])
